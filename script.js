e85ea0ee983846032082d7717e03960d7db78075
fried-hard <50778758+fried-hard@users.noreply.github.com>
2019-11-04 19:38:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,107 +4,105 @@ var inputValue = document.querySelector(".inputValue");
 var numberList = document.querySelector(".numberList");
 var listContainer = document.querySelector(".display");
 var placeHolder = document.getElementsByClassName("inputValue");
-//var hideMap = document.getElementById('map').style.display = 'none';
-//var showMap = document.getElementById('map').style.display = 'block';
 
 //initialize google map.
 var map;
+document.getElementById("map").style.display = "none";
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: new google.maps.LatLng(37.09024, -95.712891),
     mapTypeId: "terrain",
-    styles:  [
-        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#263c3f'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#6b9a76'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#38414e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#212a37'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9ca5b3'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#746855'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#1f2835'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#f3d19c'}]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{color: '#2f3948'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#17263c'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#515c6d'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#17263c'}]
-        }
-      ]
-    });
-  }
-
-
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }]
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }]
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }]
+      }
+    ]
+  });
+}
 
 //enable Enter key on submit form.
 inputValue.addEventListener("keyup", function(event) {
   event.preventDefault();
   if (event.keyCode === 13) {
     event.preventDefault();
+    document.getElementById("map").style.display = "flex";
     submitButton.click();
   }
 });
@@ -126,10 +124,8 @@ submitButton.addEventListener("click", function() {
     .then(data => {
       //narrows down to the items needed in the json reponse data.
       var venues = data.response.groups[0].items;
-      //var venueNum = venues.indexOf(venues);
-
+      
       //function that loops through to display results.
-
       function thisList(venues) {
         //listContainer.appendChild(numberList)
         for (let i = 0; i < venues.length; i++) {
@@ -142,10 +138,11 @@ submitButton.addEventListener("click", function() {
           listItem = document.createElement("li");
           listItem.innerHTML = completeList;
           numberList.appendChild(listItem);
-
+          
+          //using the iterator to number the markers on the map.
           let orderNum = i + 1;
           let orderNumtoString = orderNum.toString();
-          console.log(orderNumtoString);
+          
 
           //initializes the markers on the map and zooms in on the results.
           let marker = new google.maps.Marker({
@@ -153,7 +150,7 @@ submitButton.addEventListener("click", function() {
             map: map,
             title: venueList,
             label: orderNumtoString
-            });
+          });
           map.setZoom(13);
           map.panTo(marker.position);
         }
@@ -169,6 +166,7 @@ submitButton.addEventListener("click", function() {
     numberList.innerHTML = "";
     inputValue.value = "";
     submitButton.disabled = false;
+    document.getElementById("map").style.display = "none";
     initMap();
   });
 });
